26c77d6bb36f0757c5ccc546de83d5ef68b891a4
arshjit pelia <arsh.pelia1@gmail.com>
2023-10-14 18:23:30 -0400

    finished tutorial

--- djproj/app/models.py
+++ djproj/app/models.py
@@ -1,6 +1,11 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
-
 # Create your models here.
+
+class User(AbstractUser):
+    # Any custom fields you might want to add to the user model
+    pass
+
 class React(models.Model):
   employee = models.CharField(max_length=30)
   department = models.CharField(max_length=200)
